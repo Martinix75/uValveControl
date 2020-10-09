cb0db72be0f60ebf332123f94635aa9a351ce9d9
Martin Andrea <65339251+Martinix75@users.noreply.github.com>
2020-10-09 16:35:10 +0200

    Add files via upload

---
+++ uValveControl/uValveControl.py
@@ -0,0 +1,135 @@
+'''
+Created on 10 set 2020
+
+@author: andrea
+'''
+from pyb import Pin, delay, USB_VCP,millis
+from machine import WDT
+import time
+import uasyncio
+import martinLib
+
+class Dati(): #valori di default
+  Ton = 30
+  Toff = 800
+  T_Press = 2000
+  
+
+Seriale = USB_VCP()
+P_Apri = Pin('Y1',Pin.IN)#, Pin.PULL_UP)
+P_Chiudi = Pin('Y2',Pin.IN)#, Pin.PULL_UP)
+Valvola = Pin('Y3', Pin.OUT_PP)
+Rem_Loc = Pin('Y4', Pin.IN)
+Led_Rem = Pin('Y5',Pin.OUT_PP)
+Led_Loc = Pin('Y6',Pin.OUT_PP)
+Led_Alr = Pin('Y7',Pin.OUT_PP)
+
+#Wdt = WDT(timeout=5000) #attiva il watch dog
+
+Versione_ = '0.3 Alpha1'
+
+def Scrivi_Configurazione():
+    martinLib.Write_Config(Ton=Dati.Ton, Toff=Dati.Toff, T_Press=Dati.T_Press)
+    
+def Leggi_Configurazione(**Parametri):
+    valori = martinLib.Read_Config(**Parametri)
+    Dati.Ton = valori['Ton']
+    Dati.Toff = valori['Toff']
+    Dati.T_Press = valori['T_Press']
+
+def test(): #funzione test per i led (li accende tutti)
+    Valvola.high()
+    Led_Rem.high()
+    Led_Loc.high()
+    Led_Alr.high()
+    delay(1000)
+    Valvola.low()
+    Led_Rem.low()
+    Led_Loc.low()
+    Led_Alr.low()
+    delay(400)
+    
+async def main(mod):
+    x = uasyncio.create_task(Comunicazione(mod))# lancia la funzione in asincrono
+    await uasyncio.sleep_ms(400)# attendi un p貌...
+    x.cancel() #uccidi il processo asincrono
+
+async def Comunicazione(mod):# funzione lanciata in modo asincrono (anti blocco)
+    #if Seriale.any() == True:
+    Comando = Seriale.read()
+    Comando = Comando.decode()
+    Comando = Comando.split('=')
+    delay(300)
+    #print('Ricevuto--> ',Comando)
+    if Comando[0] == 'v_stat':
+        Seriale.send(Valvola.value())
+    elif Comando[0] == 'mod_stat':
+        Seriale.send(Rem_Loc.value())
+    elif Comando[0] == 'version':
+        Seriale.send('Ver {0}'.format(Versione_))
+    elif Comando[0] == 'test':
+        test()
+    elif Comando[0] == 't_press':
+        Dati.T_Press = int(Comando[1])
+        Apri = martinLib.Holding_Pulse(P_Apri, Dati.T_Press)
+        Chiudi = martinLib.Holding_Pulse(P_Chiudi, Dati.T_Press)
+        Scrivi_Configurazione()
+    elif Comando[0] == 'ton':
+        Dati.Ton = int(Comando[1])
+        Scrivi_Configurazione()
+        #print('ton= ',Dati.Ton)
+    elif Comando[0] == 'toff':
+        Dati.Toff = int(Comando[1])
+        Scrivi_Configurazione()
+        #print('toff= ',Dati.Toff)
+    elif Comando[0] == 'param':
+        Val_conf = martinLib.Read_Config()
+        print(Val_conf)
+    if mod == 1:
+        if Comando[0] == 'open':
+            Valvola.high()
+        elif Comando[0] == 'close':
+            Valvola.low()
+            
+def Modulo_Pulsante_Doppio():
+    #elif Rem_Loc.value() == 1: #attiva mod manuale
+    Led_Rem.low()
+    Led_Loc.high()
+    if Apri.Holding_Time() == 1:
+        Valvola.high()
+    if Chiudi.Holding_Time() == 1:
+        Valvola.low()
+'''
+def Modulo_Pulsante_Singolo():
+    Led_Rem.low()
+    Led_Loc.high()
+    if Apri.Holding_Time() == 1:
+      if Valvola.value() == 0:
+          Valvola.high()
+      elif Valvola.value() == 1:
+            Valvola.low()
+'''
+test()
+Leggi_Configurazione(Ton=Dati.Ton, Toff=Dati.Toff, T_Press=Dati.T_Press)
+Apri = martinLib.Holding_Pulse(P_Apri, Dati.T_Press)
+Chiudi = martinLib.Holding_Pulse(P_Chiudi, Dati.T_Press)
+#uasyncio.run(main(1))
+while True:
+    #Wdt.feed() #resetta conteccio watc dog
+    if Seriale.isconnected() == True:
+        martinLib.Blink(Dati.Ton, Dati.Toff, Led_Alr)
+        Seriale = USB_VCP()
+    elif Seriale.isconnected() == False:
+        Led_Alr.low()
+        Seriale.close()
+    if Rem_Loc.value() == 0:#attiva mod remota
+        Led_Rem.high()
+        Led_Loc.low()
+        if Seriale.any() == True:
+            uasyncio.run(main(1))
+
+    elif Rem_Loc.value() == 1: #attiva mod manuale
+        Modulo_Pulsante_Doppio()
+        #Modulo_Pulsante_Singolo()
+        if Seriale.any() == True:
+            uasyncio.run(main(0)) 
